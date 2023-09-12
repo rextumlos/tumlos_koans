047a946ffca6a7c87bbcaae6995d248421799e3f
rextumlos <johnrex412@gmail.com>
2023-09-12 16:30:27 +0800

    Ruby Koans upload

---
+++ triangle.rb
@@ -0,0 +1,35 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  if a <= 0 or b <= 0 or c <= 0
+    raise TriangleError.new("Negative length")
+  elsif (a + b) <= c or (a + c) <= b
+    raise TriangleError.new("Sum of 2 short sides must be greater than longest side.")
+  else
+    if a == b and b == c and a == c
+      return :equilateral
+    elsif a == b or b == c or a == c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+
+end
